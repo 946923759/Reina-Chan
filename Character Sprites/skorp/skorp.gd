b934cb11cb8495eb237115d1136bb3bf0bde0f75
AmaryllisWorks <48494956+AmaryllisWorks@users.noreply.github.com>
2022-07-08 00:32:50 -0500

    More skorpion fixes

--- Character Sprites/skorp/skorp.gd
+++ Character Sprites/skorp/skorp.gd
@@ -1,4 +1,5 @@
 extends KinematicBody2D
+signal cutscene_finished
 
 
 var gf_cutscene = preload("res://Cutscene/CutsceneMain.tscn")
@@ -10,37 +11,58 @@ export(String) var message_id = "Scarecrow1"
 var velocity:Vector2
 var gravity = 2000
 var disabled:bool=false
+var just_touched_ground:bool=false
 
+onready var animPlayer:AnimationPlayer = $AnimationPlayer
 onready var sprite:AnimatedSprite = $AnimatedSprite
 
 func _ready():
+	$LeftC.modulate.a=0
+	$RightC.modulate.a=0
+	
 	if spawnType==0:
 		velocity.y=-1000
 		#add_central_force(Vector2(0,-200))
 	sprite.flip_h=facing==1
 
+var timer:float=0
 func _physics_process(delta):
 	velocity=move_and_slide(velocity,Vector2(0,-1),true)
 	velocity.y += gravity * delta
+	
+	#Is there a smarter way to do this without having to branch?
+	if velocity.x > 0:
+		velocity.x = max(0,velocity.x-gravity*delta)
+	elif velocity.x < 0:
+		velocity.x = min(0,velocity.x+gravity*delta)
+	
 	if spawnType==0 and disabled==false and is_on_floor():
-		play_cutscene()
+		if timer>.5:
+			play_cutscene()
+		else:
+			timer+=delta
+		if just_touched_ground==false:
+			animPlayer.play("default")
+			just_touched_ground=true
 	
 	if facing==0:
 		var p = get_node("/root/Node2D").get_player()
 		sprite.flip_h=p.global_position.x < global_position.x
 	
 func play_cutscene():
-	var parent = get_parent()
-	get_tree().paused=true
-	var newCutscene = gf_cutscene.instance()
-	parent.add_child(newCutscene) #Needs to be done first for the _ready()
-	#newCutscene.connect("cutscene_finished",self,"part2")
-	newCutscene.init_(
-		Globals.get_stage_cutscene(message_id),
-		parent,
-		true,
-		null,
-		"\t"
-	)
+	if Globals.playCutscenes:
+		var parent = get_parent()
+		get_tree().paused=true
+		var newCutscene = gf_cutscene.instance()
+		parent.add_child(newCutscene) #Needs to be done first for the _ready()
+		#newCutscene.connect("cutscene_finished",self,"part2")
+		newCutscene.init_(
+			Globals.get_stage_cutscene(message_id),
+			parent,
+			true,
+			null,
+			"\t"
+		)
 	disabled=true
+	emit_signal("cutscene_finished")
 	$AudioStreamPlayer.play()
