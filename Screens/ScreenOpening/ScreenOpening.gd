749dc44ce4b350c2f999e6866f3881f37eba88ff
AmaryllisWorks <48494956+946923759@users.noreply.github.com>
2024-09-28 22:06:15 -0500

    ScreenOpening

---
+++ Screens/ScreenOpening/ScreenOpening.gd
@@ -0,0 +1,70 @@
+extends "res://Screens/ScreenWithMenuElements.gd"
+
+export(Color) var text_bg_color
+export(String) var bossToLoad
+
+onready var seq = [
+	[0.0, $ZTextWalk_NoM16],
+	[03.723, $ZText2],
+	[07.432, $Ouroboros],
+	[11.130, $Scarecrow],
+	[14.800, $Alchemist],
+	[18.450, $Architect],
+	#[20.330, ],
+	[23.100, $LogoFade],
+	[INF]
+]
+var step = 0
+onready var size = seq.size()
+var tw:SceneTreeTween
+onready var top:ColorRect = $top
+onready var bottom:ColorRect = $bottom
+
+#var total:float = 0.0
+
+func _ready():
+	if Globals.eventMode:
+		NextScreen="ScreenTitleJoin"
+		
+	for o in seq:
+		if len(o)>1:
+			o[1].visible=false
+			
+	$AudioStreamPlayer.connect("finished",self,"OffCommandNextScreen")
+	
+	tw = get_tree().create_tween()
+	tw.set_parallel()
+	#fuck you
+	VisualServer.canvas_item_set_z_index(top.get_canvas_item(),998)
+	VisualServer.canvas_item_set_z_index(bottom.get_canvas_item(),999)
+	top.visible=true
+	bottom.visible=true
+	
+	for i in range(size):
+		#var startTime:float = seq[i][0]+.1
+		#tw.tween_property(top,"rect_scale:y",0.0,.3).from(2.0).set_delay(startTime).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+		#tw.tween_property(bottom,"rect_scale:y",0.0,.3).from(2.0).set_delay(startTime).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+		
+		if i < size-1:
+			var endTime:float = seq[i+1][0] - .3
+			tw.tween_property(top,"rect_scale:y",2.0,.3).from(0.0).set_delay(endTime).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+			tw.tween_property(bottom,"rect_scale:y",2.0,.3).from(0.0).set_delay(endTime).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN)
+
+func _process(delta):
+	# Sync to playback position here
+	var total = $AudioStreamPlayer.get_playback_position()
+	if total>=seq[step][0]:
+		assert(seq[step][1])
+		#seq[step][1].OnCommand()
+		seq[step][1].OnCommand()
+		var startTime:float = seq[step][0]
+		
+		if step<size-1:
+			var newTw = get_tree().create_tween()
+			newTw.set_parallel()
+			newTw.tween_property(top,"rect_scale:y",0.0,.3).from(2.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+			newTw.tween_property(bottom,"rect_scale:y",0.0,.3).from(2.0).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_OUT)
+
+		if step>0:
+			seq[step-1][1].queue_free()
+		step+=1
